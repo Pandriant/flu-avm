2e5c36cbfa9b005152cd4fe3dc3cf7b0813a2581
Pandriant <adrianbenigaslo@gmail.com>
2026-04-22 20:28:30 +0200

    atornav: domus con listview

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  final Color electusColor;
+
+  AppTheme({
+    this.electusColor = const Color(0xFF1E1C36)
+  });
+
+  ThemeData getTheme() {
+    return ThemeData(
+      colorSchemeSeed: electusColor,
+    );
+  }
+}
