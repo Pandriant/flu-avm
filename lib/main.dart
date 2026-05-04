4514c6ade995e968269666e24d220e6c75b75c2a
Pandriant <adrianbenigaslo@gmail.com>
2026-05-04 19:43:20 +0200

    atornav: implementación del modo light/dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/providers/modus_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/config.dart';
 
@@ -10,17 +11,19 @@ void main() {
     );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final tenebrisModusEst = ref.watch(estTenebrisModusProvider);
 
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
+      theme: AppTheme( tenebrisModusEst: tenebrisModusEst, electusColor: Colors.pink.shade900).getTheme(),
       
     );
   }
