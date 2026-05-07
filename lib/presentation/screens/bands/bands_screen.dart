3e7dd3f20fd15f2a3f37ca3c4b75bdebc524c4e9
Pandriant <adrianbenigaslo@gmail.com>
2026-05-07 12:24:49 +0200

    atornav: Ventana de diálogo

--- lib/presentation/screens/bands/bands_screen.dart
+++ lib/presentation/screens/bands/bands_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import '../../../config/entities/band.dart';
 import '../../../config/config.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter/cupertino.dart';
 
 class BandsScreen extends StatelessWidget {
   const BandsScreen({super.key});
@@ -14,15 +17,91 @@ class BandsScreen extends StatelessWidget {
         body: ListView.builder(
           itemCount: bands.length,
           itemBuilder: (context, i) {
-              return ListTile(
-            leading: CircleAvatar(
-              child: Text(bands[i].nomen.substring(0,2).toUpperCase()),
-            ),
-          );
+              return _bandTile(bands[i]);
+            
+          
           },
-        )   
-    );
-    
-    
+        ),
+           floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: ()=> addereNovumBand(context),),
+    );   
   }
+  
+  ListTile _bandTile (Band band) {
+
+  return ListTile (
+
+            leading: CircleAvatar(
+              child: Text(band.nomen.substring(0,2).toUpperCase()),
+            ),
+            title: Text (band.nomen),
+            trailing: Text('${band.numerusVotum}', style: TextStyle(fontSize: 20),),
+            onTap: () {
+            },
+          );
+          }
+
+addereNovumBand(BuildContext context) {
+
+  final TextEditingController textumController = TextEditingController();
+
+ // showDialog(
+ //            context: context, 
+ //            builder: (context) {
+ //             return AlertDialog (
+ //                 title: Text('New Band Name'),
+ //                 content: TextField (controller: textumController, ),
+ //                 actions: [
+ //                   MaterialButton(
+ //                     onPressed: () => addereBandAdCollecione(context, textumController.text),
+ //                     textColor: Colors.blue,
+ //                     elevation: 5,
+ //                     child: Text('Add'),
+ //                     )
+ //                 ],
+ //             );
+ //            },
+ //           );
+
+    showCupertinoDialog(
+  context: context, 
+  builder: ( BuildContext context ) => CupertinoAlertDialog(
+    title: const Text('New band name'),
+    content:  CupertinoTextField(
+      controller: textumController,
+      style: TextStyle(
+        color: Theme.of(context).brightness == Brightness.dark 
+          ? Colors.white 
+          : Colors.black
+        )
+      ),
+      actions: [
+        CupertinoDialogAction(
+          isDefaultAction: true,
+          child: const Text('Add'),
+          onPressed: () {
+            addereBandAdCollecione(context, textumController.text);
+            context.pop();
+          }
+        ),
+        CupertinoDialogAction(
+          isDestructiveAction: true,
+          child: const Text('Close'),
+          onPressed: () => context.pop()
+        ),
+      ],
+   )
+);
+
+
+        }
+  void addereBandAdCollecione (BuildContext context, String nomen) {
+      print(nomen);
+      context.pop();
+  }
+
 }
+
+          
+  
