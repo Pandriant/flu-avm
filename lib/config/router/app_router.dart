573dc93830b2d75e3b24a601315bd447c9d07fdb
Pandriant <adrianbenigaslo@gmail.com>
2026-05-13 17:24:31 +0200

    atornav: formulario del mapa

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_application_1/presentation/screens/bands/bands_screen.dart';
 import 'package:flutter_application_1/presentation/screens/domus/domus_screen.dart';
+import 'package:flutter_application_1/presentation/screens/charta/charta_screen.dart';
 import 'package:flutter_application_1/presentation/screens/numerator/numerator_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -18,6 +19,10 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/bands',
       builder:(context, state) => const  BandsScreen(),
+      ),
+    GoRoute(
+      path: '/charta',
+      builder:(context, state) => const  ChartaScreen(),
       ),
   ]
   );
