0b1054faa2f534a82b5e69f920c17ef870b3399b
Pandriant <adrianbenigaslo@gmail.com>
2026-05-25 04:11:29 +0200

    atornav: Petición http y mapeado de la respuesta

---
+++ lib/presentation/providers/pokemon_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flu_avm/config/config.dart';
+import 'package:flu_avm/services/pokemon_services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final pokemonProvider = FutureProvider.family<Pokemon, String>((ref, id) async{
+  final (pokemon, error) =  await PokemonServices.getPokemon(id);
+
+  if(pokemon != null) return pokemon;
+
+  throw Exception(error);
+});
